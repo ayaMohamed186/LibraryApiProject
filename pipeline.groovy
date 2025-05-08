161ad47ce40858e687c0c706b64b7b1f5627a2e0
ayaMohamed186 <37931347+ayaMohamed186@users.noreply.github.com>
2025-05-08 18:19:58 +0300

    add pipeline groovy file

---
+++ pipeline.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Get Code') {
+            steps {
+                git changelog: false, poll: false, url: 'https://github.com/ayaMohamed186/LibraryApiProject.git'
+            }
+        }
+
+        stage('Run Test') {
+            steps {
+                bat 'newman run LibraryCollection.postman_collection.json -e dev.postman_environment.json -r htmlextra'
+            }
+        }
+    }
+}
